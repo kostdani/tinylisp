e8368a60d53c755b1324f7eb2fe030834bd9ced6
Danila Kost <kostdani@fit.cvut.cz>
2022-11-06 00:48:09 +0100

    Update hw_racket/matrix.rkt

--- hw_racket/matrix.rkt
+++ hw_racket/matrix.rkt
@@ -1,6 +1,5 @@
 
 (define (matrix-mul a b)
-(if (and (equal? a b) (equal? a '(()))) a
 (map (lambda (ae)
-(foldl (lambda (x y) (map + x y)) (map (lambda (x) 0) a)
-(map (lambda (x y) (map (lambda (z) (* x z)) y)) ae b))) a)))
+(foldl (lambda (x y) (map + x y)) (map (lambda (x) 0) (car b))
+(map (lambda (x y) (map (lambda (z) (* x z)) y)) ae b))) a))
