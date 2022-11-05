e896303f10dc4ea6a4213568e603085175d3d494
Danila Kost <kostdani@fit.cvut.cz>
2022-11-05 23:58:14 +0100

    Update hw_racket/intervals.rkt

--- hw_racket/intervals.rkt
+++ hw_racket/intervals.rkt
@@ -2,5 +2,5 @@
 (define (len l)
   (foldl (lambda (x y) (+ 1 y)) 0 l))
 (define (intervals l)
-(map (lambda (x) (- (len (filter x l)) 1)) 
+(map (lambda (x) (filter x l))
 (map (lambda (a) (lambda (b) (and (<= (car a) (cadr b)) (>= (cadr a) (car b))))) l)))
