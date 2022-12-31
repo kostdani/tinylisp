0d9d4059f06f4ff1fe655f40a0feca3cf5dce9ca
kostdani <kostdani@fit.cvut.cz>
2022-12-31 13:03:30 +0100

    fixes

--- Runtime.cpp
+++ Runtime.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "Runtime.h"
+
+SEXP Runtime::read(){
+    return parser.Parse();
+}
+SEXP Runtime::eval(SEXP v){
+
+    std::queue<CODE> code;
+
+    auto s=v->PartialEval(env,0);
+    s->toRPN(code);
+    auto sg=s.get();
+    secd.code=code;
+    return secd.eval();
+}
+void Runtime::print(SEXP v){
+    out<<*v<<std::endl;
+}
+
+void Runtime::repl(){
+    while(true) {
+        try {
+            SEXP r=read();
+            if(parser.CurTok==END)
+                break;
+            print(eval(r));
+        }
+        catch (std::string err) {
+            out << "ERROR: " << err << std::endl;
+        }
+        secd=SECD();
+    }
+}
+
+Environment StandartRuntime::stdenv(){
+    Environment env;
+    env.addSymb("if",std::make_shared<Symbol>("if"));
+    env.addSymb("lambda",std::make_shared<Symbol>("lambda"));
+    env.addSymb("quote",std::make_shared<Symbol>("quote"));
+    env.addSymb("+",std::make_shared<Builtin>(std::make_shared<ADD>()));
+    env.addSymb("*",std::make_shared<Builtin>(std::make_shared<MUL>()));
+    env.addSymb("-",std::make_shared<Builtin>(std::make_shared<SUB>()));
+    env.addSymb("cons",std::make_shared<Builtin>(std::make_shared<CONS>()));
+    return env;
+}
--- Runtime.h
+++ Runtime.h
@@ -8,58 +8,22 @@
 #include "FrontEnd.h"
 #include "SECD.h"
 class Runtime {
-Parser parser;
-std::ostream &out;
-Environment env;
-SECD secd;
-SEXP read(){
-    return parser.Parse();
-}
-SEXP eval(SEXP v){
-
-    std::queue<CODE> code;
-
-    auto s=v->PartialEval(env,0);
-    s->toRPN(code);
-    auto sg=s.get();
-    secd.code=code;
-    return secd.eval();
-}
-void print(SEXP v){
-    out<<*v<<std::endl;
-}
+    Parser parser;
+    std::ostream &out;
+    Environment env;
+    SECD secd;
+    SEXP read();
+    SEXP eval(SEXP v);
+    void print(SEXP v);
 public:
     Runtime(std::istream& in,std::ostream &out,Environment env): parser(in),out(out),env(env){}
-    void repl(){
-        while(true) {
-            try {
-                SEXP r=read();
-                if(parser.CurTok==END)
-                    break;
-                print(eval(r));
-            }
-            catch (std::string err) {
-                out << "ERROR: " << err << std::endl;
-            }
-            secd=SECD();
-        }
-    }
+    void repl();
 };
 
 
 class StandartRuntime: public Runtime{
-    Environment sdenv(){
-        Environment env;
-        env.addSymb("if",std::make_shared<Symbol>("if"));
-        env.addSymb("lambda",std::make_shared<Symbol>("lambda"));
-        env.addSymb("quote",std::make_shared<Symbol>("quote"));
-        env.addSymb("+",std::make_shared<Builtin>(std::make_shared<ADD>()));
-        env.addSymb("*",std::make_shared<Builtin>(std::make_shared<MUL>()));
-        env.addSymb("-",std::make_shared<Builtin>(std::make_shared<SUB>()));
-        env.addSymb("cons",std::make_shared<Builtin>(std::make_shared<CONS>()));
-        return env;
-    }
+    Environment stdenv();
 public:
-    StandartRuntime(): Runtime(std::cin,std::cout,sdenv()){}
+    StandartRuntime(): Runtime(std::cin,std::cout,stdenv()){}
 };
 #endif //TINYLIST_RUNTIME_H
